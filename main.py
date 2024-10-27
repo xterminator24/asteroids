37d95b4b8699f05c76980056f5d25dc29b5ae312
xterminator24 <scott.mckillop@outlook.com>
2024-10-27 12:26:07 -0300

    add main boilerplate

---
+++ main.py
@@ -0,0 +1,10 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+def main():
+  print("Starting asteroids!")
+
+if __name__ == "__main__":
+  main()
