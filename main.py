2d2224690c7324283aae918b06b87124ba2e9a4e
xterminator24 <scott.mckillop@outlook.com>
2024-10-27 13:06:03 -0300

    refactor game loop

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ def main():
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
   while True:
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
-
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+
+    screen.fill("black")
+    pygame.display.flip()
+
 
 if __name__ == "__main__":
   main()
