4f3e58b32c59ac3818eeb0baa308b683c3e1160f
xterminator24 <scott.mckillop@outlook.com>
2024-10-27 14:42:36 -0300

    organize sprites into groups

--- main.py
+++ main.py
@@ -11,17 +11,26 @@ def main():
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
   dt = 0
-  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+
+  Player.containers = (updatable, drawable)
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+  
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
-      
-    player.update(dt)
 
+    for sprite in updatable:
+      sprite.update(dt)
+      
     screen.fill("black")
-    player.draw(screen)    
+
+    for sprite in drawable:
+      sprite.draw(screen)
+    
     pygame.display.flip()
 
     # limit the framerate to 60 FPS
